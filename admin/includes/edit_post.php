3a88beaebc41c06a83bb551aaa6c28f9dbe62446
Lina Liao <lina.liao18@gmail.com>
2020-07-30 21:54:34 -0700

    create,insert,delete post in admin

---
+++ admin/includes/edit_post.php
@@ -0,0 +1,43 @@
+<form action="" method="post" enctype="multipart/form-data">
+
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="title">
+    </div>
+
+    <div class="form-group">
+        <label for="post_category">Post Category ID</label>
+        <input type="text" class="form-control" name="post_category_id">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Title</label>
+        <input type="text" class="form-control" name="author">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Status</label>
+        <input type="text" class="form-control" name="post_status">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Images</label>
+        <input type="file" class="form-control" name="image">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Post Tags</label>
+        <input type="text" class="form-control" name="post_tags">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+    </div>
+
+</form>
+    
