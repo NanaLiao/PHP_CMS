081a39cb57ae9677650b10a9d16d0b47c32954ef
Lina Liao <lina.liao18@gmail.com>
2020-08-03 21:26:05 -0700

    insert new fields in Front end comment form

--- post.php
+++ post.php
@@ -53,15 +53,42 @@
            <?php } ?>
 
              <!-- Blog Comments -->
+             <?php
+             if(isset($_POST['create_comment'])){
+                 $comment_author = $_POST['comment_author'];
+                 $comment_email = $_POST['comment_email'];
+                 $comment_content = $_POST['comment_content'];
+                 echo $comment_author;
+
+             }
+
+
+
+            ?>
+
+
+
+
+
 
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form role="form" action="" method="post">
+
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                        <label for="Author">Author</label>
+                            <input type="text" class="form-control" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                        <label for="Email">Email</label>
+                            <input type="email" class="form-control" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                        <label for="comment">Your comment</label>
+                            <textarea class="form-control" name="comment_content" rows="3"></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
